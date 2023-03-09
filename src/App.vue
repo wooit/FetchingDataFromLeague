e3222e987ec72104c9163a63999dabb0f9ced1db
geo <geoboiffils34@gmail.com>
2023-03-09 15:38:34 +0100

    init project with vue router, vuex

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Welcome</h1>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue';
+import App from '@/App';
+import store from "@/store/index";
+import router from "@/router";
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+
+app.mount('#app')
