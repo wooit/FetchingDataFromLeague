e79cbbb928a3d3e6d37190f10b79a626b4b75793
geo <geoboiffils34@gmail.com>
2023-03-16 16:06:16 +0100

    feature: all data regarding status servers have been fetches and displayed (not properly) feature: update store where I added regions

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
 import {createStore} from "vuex";
 import championsModule from "@/store/modules/champions";
+import serverStatusModule from "@/store/modules/serversStatus";
+
 const store = createStore({
     modules: {
-        champions: championsModule
+        champions: championsModule,
+        serverStatus: serverStatusModule
     },
     state(){
         return {
-
+            apiKey: process.env.VUE_APP_API_LEAGUE_OF_LEGENDS_KEY
+        }
+    },
+    getters: {
+        getApiKey(state){
+            return state.apiKey
         }
     }
 })
---
+++ src/store/modules/serversStatus/getters.js
@@ -0,0 +1,5 @@
+export default {
+    getAllRegions(state){
+        return state.regions
+    }
+}
