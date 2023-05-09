40e6471da64f11f709d7851ddcb51ee0470d3957
geo <geoboiffils34@gmail.com>
2023-05-09 12:32:11 +0200

    fix: fixed navbar and footer css , added logo, created assets folder for img and mp4

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container-home">
 
-    <video-background class="animated-background" src="kda.mp4" style="max-height: 100vh; height: 100vh;">
+    <video-background class="animated-background" src="/assets/kda.mp4" style="max-height: 100vh; height: 100vh;">
       <section>
         <p>
           Discover all<span @click="routeTo('champions')"> champions </span>available, learn more about their lore, capacities, skins ...
@@ -11,7 +11,7 @@
 
     <div class="section-separator"></div>
 
-    <video-background class="animated-background" src="leona.mp4" style="max-height: 100vh; height: 100vh;">
+    <video-background class="animated-background" src="/assets/leona.mp4" style="max-height: 100vh; height: 100vh;">
       <section>
         <p>
           Find out more information about a <span @click="routeTo('summoner')"> summoner </span>, his level mastery for each champion, his match history ...
@@ -21,7 +21,7 @@
 
     <div class="section-separator"></div>
 
-    <video-background class="animated-background" src="zed.mp4" style="max-height: 100vh; height: 100vh;">
+    <video-background class="animated-background" src="/assets/zed.mp4" style="max-height: 100vh; height: 100vh;">
       <section>
         <p>
           Checkout who is wrecking the <span @click="routeTo('ranking')"> ladder </span> in your region ...
@@ -31,7 +31,7 @@
 
     <div class="section-separator"></div>
 
-    <video-background class="animated-background" src="sona.mp4" style="max-height: 100vh; height: 100vh;">
+    <video-background class="animated-background" src="/assets/sona.mp4" style="max-height: 100vh; height: 100vh;">
       <section>
         <p>
           See which champions are available in the weekly <span @click="routeTo('rotation')"> rotation </span> in your region ...
@@ -41,7 +41,7 @@
 
     <div class="section-separator"></div>
 
-    <video-background class="animated-background" src="diana.mp4" style="max-height: 100vh; height: 100vh;">
+    <video-background class="animated-background" src="/assets/diana.mp4" style="max-height: 100vh; height: 100vh;">
       <section>
         <p>
           Stay updated about the current <span @click="routeTo('status-servers')"> status </span> of the server you are playing on ...
